faa80bc07451e3a4ed3e9ec7079356276b9d6d6f
StanMDev <github@wreg.ch>
2019-06-15 17:08:10 +0200

    Added Good bye message

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -283,6 +283,7 @@ def main():
 
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
+            print("\nThank you for your interest in bike sharing data.") 
             break
         else:
             #clean sreen for new start
